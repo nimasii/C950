2493b182a8bb6558050798313ae5e5385a637720
Jacob Campbell <103970928+nimasii@users.noreply.github.com>
2023-10-18 17:29:14 +0000

    Started work on project

---
+++ package.py
@@ -0,0 +1,26 @@
+# Create class for packages
+class Package:
+    def __init__(self, id, address, city, state, zipcode, deadline_time, weight, status):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.deadline_time = deadline_time
+        self.weight = weight
+        self.status = status
+        self.departure_time = None
+        self.delivery_time = None
+
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.id, self.address, self.city, self.state, self.zipcode,
+                                                       self.deadline_time, self.weight, self.delivery_time,
+                                                       self.status)
+
+    def update_status(self, convert_timedelta):
+        if self.delivery_time < convert_timedelta:
+            self.status = "Delivered"
+        elif self.departure_time > convert_timedelta:
+            self.status = "En route"
+        else:
+            self.status = "At Hub"
